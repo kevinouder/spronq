56c652118eee6fe11de01588744cd4716a79c2d1
Kevin <kevin@cube.nl>
2022-01-10 20:59:04 +0100

    Get data from api, solved styling for leaflet and add markers to map

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
-import { Map, TileLayer } from 'leaflet';
+import { Map, Marker, TileLayer } from 'leaflet';
+import mushrooms, { Mushroom as MushroomInterface } from './api/api';
 
 import './style.css'
+import 'leaflet/dist/leaflet.css';
 
+//Define globals.
 const mapElm = document.querySelector<HTMLDivElement>('#map')!;
+let map: Map;
+let mushroomData: MushroomInterface[] | never[] = [];
 
-const initMap = () => {
-  const map = new Map(mapElm, {
+/**
+ * Initialize openstreetmap map.
+ * 
+ * @returns void
+ */
+const initMap = (): void => {
+  map = new Map(mapElm, {
     center: [52.082042, 5.236192],
     zoom: 17,
   });
@@ -17,4 +27,30 @@ const initMap = () => {
   map.addLayer(layer);
 };
 
+/**
+ * Add all markers to the map.
+ *
+ * @return void
+ */
+ const placeMapMarkers = (): void => {
+  mushroomData.forEach((mushroom: MushroomInterface) => {
+    new Marker(mushroom.latlng)
+      .addTo(map)
+      .bindPopup(mushroom.name);
+  });
+};
+
+/**
+ * Get mushroom data from the api.
+ *
+ * @returns Promise<MushroomInterface[]>
+ */
+ const getMushroomData = async (): Promise<MushroomInterface[]> => {
+  return await mushrooms()
+    .then((data) => mushroomData = data)
+    .catch(() => []);
+};
+
 initMap();
+await getMushroomData();
+placeMapMarkers();
