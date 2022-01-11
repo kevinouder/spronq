a6812b28da208bfab79bced902d66ab75e5547b3
Kevin <kevin@cube.nl>
2022-01-11 09:16:25 +0100

    Moved some function to helpers

--- src/main.ts
+++ src/main.ts
@@ -4,29 +4,28 @@ import mushrooms, {
   Color,
   Spots,
 } from './api/api';
-import { EnumInterface, OptionInterface } from './types';
+import { generatePopUpHTML, convertEnumToArray, createFilter } from './providers/helpers';
 
 import './style.css';
 import 'leaflet/dist/leaflet.css';
 
 //Define globals.
 const mapElm = document.querySelector<HTMLDivElement>('#map')!;
-const loadingElm =
-  document.querySelector<HTMLDivElement>('#loading-mushrooms')!;
+const loadingElm = document.querySelector<HTMLDivElement>('#loading-mushrooms')!;
 const filtersElm = document.querySelector<HTMLDivElement>('#filters')!;
 const colorsFilterElm = document.querySelector<HTMLSelectElement>('#colors')!;
 const spotsFilterElm = document.querySelector<HTMLSelectElement>('#spots')!;
+
+let map: Map;
+let initializeMushroomData: MushroomInterface[] | never[] = [];
+let mushroomData: MushroomInterface[] | never[] = [];
+let markerLayer: LayerGroup;
 
 const mushroomIcon = new Icon({
   iconUrl: './src/images/mushroom.svg',
   iconSize: [66, 64], // size of the icon
   popupAnchor: [0, -28], // point from which the popup should open relative to the iconAnchor
 });
-
-let map: Map;
-let initializeMushroomData: MushroomInterface[] | never[] = [];
-let mushroomData: MushroomInterface[] | never[] = [];
-let markerLayer: LayerGroup;
 
 /**
  * Initialize openstreetmap map.
@@ -44,22 +43,6 @@ const initMap = (): void => {
 
   markerLayer = new LayerGroup();
   map.addLayer(markerLayer);
-};
-
-/**
- * Create html string based on mushroom.
- *
- * @param mushroom
- * @returns string
- */
-const generatePopUpHTML = (mushroom: MushroomInterface): string => {
-  let popUpHtml: string = '<ul class="popup-list">';
-  popUpHtml += `<li class="capitalize">Name: ${mushroom.name}</li>`;
-  popUpHtml += `<li class="capitalize">Spots: ${Spots[mushroom.spots]}</li>`;
-  popUpHtml += `<li class="capitalize">Color: ${Color[mushroom.color]}</li>`;
-  popUpHtml += '</ul>';
-
-  return popUpHtml;
 };
 
 /**
@@ -131,52 +114,6 @@ const getMushroomData = async (): Promise<MushroomInterface[]> => {
 };
 
 /**
- * Create filter and append it to filtersWrapElm
- *
- * @param filterElm
- * @param options
- * @return void
- */
-const createFilter = (
-  filterElm: HTMLSelectElement,
-  options: OptionInterface[]
-): void => {
-  // Add options to select element.
-  options.forEach((object: OptionInterface) => {
-    const option: HTMLOptionElement = document.createElement('option');
-    option.value = object.value.toString();
-    option.label = object.label
-      .toLowerCase()
-      .replace(/^./, object.label[0].toUpperCase());
-
-    filterElm.add(option);
-  });
-
-  const buttonElm: HTMLButtonElement | never = filterElm?.nextElementSibling as HTMLButtonElement;
-  buttonElm?.addEventListener('click', () => {
-    filterElm.value = ''
-    filterMushrooms()
-  });
-
-  // Add on change eventlistener.
-  filterElm.addEventListener('change', filterMushrooms);
-};
-
-/**
- * Convert TS Enum To Array of Objects.
- *
- * @param e
- * @return OptionInterface[]
- */
-const convertEnumToArray = (e: EnumInterface): OptionInterface[] =>
-  Object.values(e)
-    .filter((value) => typeof value === 'string')
-    .map((label, index) => ({
-      label,
-      value: index.toString(),
-    }));
-
-/**
  * Init function.
  *
  * @return void
@@ -185,8 +122,8 @@ const convertEnumToArray = (e: EnumInterface): OptionInterface[] =>
   initMap();
   await getMushroomData();
   placeMapMarkers();
-  createFilter(colorsFilterElm, convertEnumToArray(Color));
-  createFilter(spotsFilterElm, convertEnumToArray(Spots));
+  createFilter(colorsFilterElm, convertEnumToArray(Color), filterMushrooms);
+  createFilter(spotsFilterElm, convertEnumToArray(Spots), filterMushrooms);
 };
 
 init();
---
+++ src/providers/helpers.ts
@@ -0,0 +1,66 @@
+import { Mushroom as MushroomInterface, Color, Spots } from '../api/api';
+import { EnumInterface, OptionInterface } from '../types';
+
+/**
+ * Convert TS Enum To Array of Objects.
+ *
+ * @param e
+ * @return OptionInterface[]
+ */
+ export const convertEnumToArray = (e: EnumInterface): OptionInterface[] =>
+ Object.values(e)
+   .filter((value) => typeof value === 'string')
+   .map((label, index) => ({
+     label,
+     value: index.toString(),
+   }));
+
+/**
+ * Create html string based on mushroom.
+ *
+ * @param mushroom
+ * @returns string
+ */
+ export const generatePopUpHTML = (mushroom: MushroomInterface): string => {
+  let popUpHtml: string = '<ul class="popup-list">';
+  popUpHtml += `<li class="capitalize">Name: ${mushroom.name}</li>`;
+  popUpHtml += `<li class="capitalize">Spots: ${Spots[mushroom.spots]}</li>`;
+  popUpHtml += `<li class="capitalize">Color: ${Color[mushroom.color]}</li>`;
+  popUpHtml += '</ul>';
+
+  return popUpHtml;
+};
+
+/**
+ * Create filter and append it to filtersWrapElm
+ *
+ * @param filterElm
+ * @param options
+ * @return void
+ */
+ export const createFilter = (
+  filterElm: HTMLSelectElement,
+  options: OptionInterface[],
+  filterFunc: Function,
+): void => {
+  // Add options to select element.
+  options.forEach((object: OptionInterface) => {
+    const option: HTMLOptionElement = document.createElement('option');
+    option.value = object.value.toString();
+    option.label = object.label
+      .toLowerCase()
+      .replace(/^./, object.label[0].toUpperCase());
+
+    filterElm.add(option);
+  });
+
+  // Add on click eventlistener to reset button.
+  const buttonElm: HTMLButtonElement | never = filterElm?.nextElementSibling as HTMLButtonElement;
+  buttonElm?.addEventListener('click', () => {
+    filterElm.value = ''
+    filterFunc()
+  });
+
+  // Add on change eventlistener.
+  filterElm.addEventListener('change', () => filterFunc());
+};
